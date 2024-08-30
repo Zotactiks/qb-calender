43bdfc0087d7854019d39b28ccb87d4f71a9eae5
Zotactiks <101468344+Zotactiks@users.noreply.github.com>
2024-08-30 23:53:55 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'YourName'
+description 'QB-Calendar'
+version '1.0.0'
+
+client_script 'client/main.lua'
+server_script 'server/main.lua'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js',
+    'html/images/*.jpg'
+}
